e0e3bf984dcc40e0e61c4ee6f51ec4eb4fa5ee2f
Samuel Bucheli <samuel.bucheli@zuehlke.com>
2019-10-15 14:38:47 +0200

    Adding simple CPU test

---
+++ tests/CpuTests.elm
@@ -0,0 +1,33 @@
+module CpuTests exposing (..)
+
+import Array
+import Cpu exposing (apply)
+import CpuState exposing (ConditionCodes, CpuState, MachineStateDiff, MachineStateDiff(..), MachineStateDiffEvent(..))
+import Expect
+import OpCode exposing (MachineState(..))
+import Test exposing (..)
+
+all : Test
+all =
+    describe "Cpu"
+    [ describe "apply"
+      [ test "applies diff events from left to right" <|
+        \() ->
+            let
+              machineStateDiff =  Events [ SetRegisterB 11, SetRegisterB 22 ]
+              initialCpuState = CpuState 0 0 0 0 0 0 0 0 0 Array.empty (ConditionCodes False False False False False) False
+              updatedCpuState = apply machineStateDiff initialCpuState
+              valueOfRegisterB = getValueOfRegisterB updatedCpuState
+            in
+              Expect.equal (Just 22) valueOfRegisterB
+
+      ]
+    ]
+
+getValueOfRegisterB : MachineState -> Maybe Int
+getValueOfRegisterB machineState =
+    case machineState of
+        Valid cpuState -> Just (cpuState.b)
+
+
+        Invalid _ _ -> Nothing
