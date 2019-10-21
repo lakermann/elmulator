bcfb136e25a25f8b8376a4288aae75cfe16853c3
Lukas Akermann <Lukas.Akermann@zuehlke.com>
2019-10-21 21:55:31 +0200

    Add test for inx_b

--- tests/MachineInstructionsTest.elm
+++ tests/MachineInstructionsTest.elm
@@ -30,7 +30,7 @@ all =
                 \() ->
                     let
                         expectedMachineStateDiff =
-                            Events [ SetCpu (SetPC 1) ]
+                            Events [ SetCpu (SetPC 0x01) ]
                     in
                     Expect.equal expectedMachineStateDiff (MachineInstructions.nop allZeroMachineState)
             ]
@@ -39,29 +39,48 @@ all =
                 \() ->
                     let
                         expectedMachineStateDiff =
-                            Events [ SetCpu (SetRegisterB 3), SetCpu (SetRegisterC 2), SetCpu (SetPC 3) ]
+                            Events [ SetCpu (SetRegisterB 0x03), SetCpu (SetRegisterC 0x02), SetCpu (SetPC 0x03) ]
                     in
-                    Expect.equal expectedMachineStateDiff (MachineInstructions.lxi_b_d16 2 3 allZeroMachineState)
+                    Expect.equal expectedMachineStateDiff (MachineInstructions.lxi_b_d16 0x02 0x03 allZeroMachineState)
             ]
         , describe "0x02 - stax_b"
-            [ test "for zero machine state" <|
+            [ test "for a=0x01, b=0x02, c=0x03 machine state" <|
                 \() ->
                     let
                         a =
-                            1
+                            0x01
 
                         b =
-                            2
+                            0x02
 
                         c =
-                            3
+                            0x03
 
                         machineState =
                             { allZeroMachineState | cpuState = CpuState a b c 0 0 0 0 0 0 allFalseConditionCodes False 0 }
 
                         expectedMachineStateDiff =
-                            Events [ SetMemory (b * 256 + c) a, SetCpu (SetPC 1) ]
+                            Events [ SetMemory (b * 0x0100 + c) a, SetCpu (SetPC 1) ]
                     in
                     Expect.equal expectedMachineStateDiff (MachineInstructions.stax_b machineState)
+            ]
+        , describe "0x03 - inx_b"
+            [ test "for zero machine state" <|
+                \() ->
+                    let
+                        expectedMachineStateDiff1 =
+                            Events [ SetCpu (SetRegisterC 1), SetCpu (SetPC 1) ]
+                    in
+                    Expect.equal expectedMachineStateDiff1 (MachineInstructions.inx_b allZeroMachineState)
+            , test "for c=0xFF machine state" <|
+                \() ->
+                    let
+                        machineState =
+                            { allZeroMachineState | cpuState = CpuState 0 0 0xFF 0 0 0 0 0 0 allFalseConditionCodes False 0 }
+
+                        expectedMachineStateDiff2 =
+                            Events [ SetCpu (SetRegisterB 1), SetCpu (SetRegisterC 0), SetCpu (SetPC 1) ]
+                    in
+                    Expect.equal expectedMachineStateDiff2 (MachineInstructions.inx_b machineState)
             ]
         ]
