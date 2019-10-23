9daed89200534e33729d47f1577bf3d33aa26e03
Lukas Akermann <Lukas.Akermann@zuehlke.com>
2019-10-23 07:32:54 +0200

    Add tests for mov_e_m

--- tests/MachineInstructionsTest.elm
+++ tests/MachineInstructionsTest.elm
@@ -551,4 +551,25 @@ all =
                     in
                     Expect.equal expectedMachineStateDiff (MachineInstructions.mov_d_m machineState)
             ]
+        , describe "0x5e - mov_e_m"
+            [ test "for h=0x01.l=0x02 machine state" <|
+                \() ->
+                    let
+                        h =
+                            0x01
+
+                        l =
+                            0x02
+
+                        machineState =
+                            { allZeroMachineState | cpuState = CpuState 0 0 0 0 0 h l 0 0 allFalseConditionCodes False 0, memory = fromList (range 0 0x0102) }
+
+                        expectedMachineStateDiff =
+                            Events
+                                [ SetCpu (SetRegisterE 0x0102)
+                                , SetCpu (SetPC 0x01)
+                                ]
+                    in
+                    Expect.equal expectedMachineStateDiff (MachineInstructions.mov_e_m machineState)
+            ]
         ]
