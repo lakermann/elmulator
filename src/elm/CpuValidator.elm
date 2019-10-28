5df5ad45672fd77983f5cb5e51cea15060357d1e
Lukas Akermann <Lukas.Akermann@zuehlke.com>
2019-10-28 20:46:45 +0100

    Add validation for step 1000

--- src/elm/CpuValidator.elm
+++ src/elm/CpuValidator.elm
@@ -102,6 +102,38 @@ validate machineState =
                             /= False
                        )
                )
+            || (machineState.step
+                    == 1000
+                    && (machineState.cpuState.a
+                            /= 0x14
+                            || machineState.cpuState.b
+                            /= 0x5B
+                            || machineState.cpuState.c
+                            /= 0x00
+                            || machineState.cpuState.d
+                            /= 0x1B
+                            || machineState.cpuState.e
+                            /= 0xA5
+                            || machineState.cpuState.h
+                            /= 0x20
+                            || machineState.cpuState.l
+                            /= 0xA5
+                            || machineState.cpuState.pc
+                            /= 0x1A32
+                            || machineState.cpuState.sp
+                            /= 0x23FE
+                            || machineState.cpuState.conditionCodes.ac
+                            /= False
+                            || machineState.cpuState.conditionCodes.cy
+                            /= False
+                            || machineState.cpuState.conditionCodes.s
+                            /= False
+                            || machineState.cpuState.conditionCodes.p
+                            /= False
+                            || machineState.cpuState.conditionCodes.z
+                            /= False
+                       )
+               )
     then
         Invalid (Just machineState) ("Invalid state, step=" ++ String.fromInt machineState.step)
 
